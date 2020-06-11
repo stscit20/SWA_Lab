c5e58df69cd6ceabf2b31aec6a0bfaa6ffa0facf
skn219 <bbini5@hotmail.de>
2020-06-11 17:12:45 +0200

    ServicecontractResources Test done

--- src/test/java/de/hse/swa/SWA_Lab/resources/ServicecontractResourceTest.java
+++ src/test/java/de/hse/swa/SWA_Lab/resources/ServicecontractResourceTest.java
@@ -1,4 +1,99 @@
 package de.hse.swa.SWA_Lab.resources;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.Test;
+
+import de.hse.swa.SWA_Lab.model.*;
+import de.hse.swa.SWA_Lab.dao.UserDao;
+import de.hse.swa.SWA_Lab.dao.CompanyDao;
+import de.hse.swa.SWA_Lab.dao.ServicecontractDao;
+import de.hse.swa.SWA_Lab.PrepareTests;
+import de.hse.swa.SWA_Lab.resources.*;
 
 public class ServicecontractResourceTest {
+private static Servicecontract servicecontract;
+	private static ServicecontractResource servicecontractresource;
+	
+	private static Company company = null;
+	private static Swauser swauser = null;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		//PrepareTests.initDatabase();
+	//    company = new Company();
+	//	company.setCompanyname("TestCompany");
+		 
+	//	CompanyDao.getInstance().saveCompany(company);
+		
+		
+	//	swauser = new Swauser();
+	//	swauser.setUsername("Test Name");
+	//	swauser.setPassword("test");
+	//	swauser.setCompany(company);
+	
+	    UserDao.getInstance().saveSwauser(swauser);
+}
+	
+	
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	
+	    company = new Company();
+		company.setCompanyname("TestCompany");
+		CompanyDao.getInstance().saveCompany(company);
+		Swauser user =new Swauser();
+		user.setUsername("test");
+		user.setCompany(company);
+		UserDao.getInstance().saveSwauser(user);;
+		servicecontract = new Servicecontract();
+		servicecontract.setCompany(company);
+		servicecontract.setSwauser(user);
+		ServicecontractDao.getInstance().saveServicecontract(servicecontract);
+		servicecontractresource =new ServicecontractResource();
+	}
+	public void tearDown() throws Exception {
+	}
+
+	@Rule 
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void testGetServicecontract(){
+		assertEquals(servicecontractresource.getServicecontract(1),servicecontract);
+	}
+	
+	@Test
+	public void testGetServicecontracttHtml(){
+		try{
+			servicecontractresource.getServicecontractHTML(servicecontract.getIdservicecontract());
+		}
+		catch(RuntimeException e){
+			assertTrue(false);
+		}
+		assertTrue(true);
+		
+	}
+	
+	@Test
+	public void testDeleteServicecontract(){
+		servicecontractresource.deleteServicecontract(servicecontract.getIdservicecontract());
+		thrown.expect(RuntimeException.class);
+		thrown.expectMessage("Get: Servicecontract with " + servicecontract.getIdservicecontract() +  " not found");
+		servicecontractresource.getServicecontractHTML(servicecontract.getIdservicecontract());
+	}
 }
--- src/test/java/de/hse/swa/SWA_Lab/resources/ServicecontractsResourceTest.java
+++ src/test/java/de/hse/swa/SWA_Lab/resources/ServicecontractsResourceTest.java
@@ -1,4 +1,127 @@
 package de.hse.swa.SWA_Lab.resources;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.BeforeClass;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.hse.swa.SWA_Lab.PrepareTests;
+import de.hse.swa.SWA_Lab.model.Company;
+import de.hse.swa.SWA_Lab.model.Servicecontract;
+import de.hse.swa.SWA_Lab.model.Swauser;
+import de.hse.swa.SWA_Lab.resources.*;
+import de.hse.swa.SWA_Lab.dao.*;
+
+
 public class ServicecontractsResourceTest {
+	private static Company company = null;
+	private static Swauser swauser = null;
+	
+	private static ServicecontractDao s;
+	
+@BeforeClass
+public static void setUpBeforeClass() throws Exception {
+	s = ServicecontractDao.getInstance();
+    company = new Company();
+	company.setCompanyname("TestCompany");
+	 
+	CompanyDao.getInstance().saveCompany(company);
+	
+	
+	swauser = new Swauser();
+	swauser.setUsername("Test Name");
+	swauser.setPassword("test");
+	swauser.setCompany(company);
+
+    UserDao.getInstance().saveSwauser(swauser);
+}
+
+
+@AfterClass
+public static void tearDownAfterClass() throws Exception {
+}
+
+@Before
+public void setUp() throws Exception {
+	
+}
+
+@After
+public void tearDown() throws Exception {
+}
+
+
+
+@Test
+public void testGetServicecontracts(){
+    ServicecontractsResource servicecontractsResource = new ServicecontractsResource();
+    int count1 = servicecontractsResource.getAllServicecontract().size();
+    Servicecontract servicecontract= new Servicecontract();
+     servicecontract.setCompany(company);
+     servicecontract.setSwauser(swauser);
+    ServicecontractDao.getInstance().saveServicecontract(servicecontract);
+
+    int count2 = servicecontractsResource.getAllServicecontract().size();
+    assertEquals(count2-count1, 1);
+}
+
+@Test
+public void testGetCount(){
+    ServicecontractsResource ServicecontractsResource = new ServicecontractsResource();
+    int count1 = Integer.parseInt(ServicecontractsResource.getCount());
+    Servicecontract servicecontract= new Servicecontract();
+    servicecontract.setCompany(company);
+    servicecontract.setSwauser(swauser);
+    ServicecontractDao.getInstance().saveServicecontract(servicecontract);
+    CompanyDao.getInstance().saveCompany(company);
+
+    int count2 = Integer.parseInt(ServicecontractsResource.getCount());
+    assertEquals(count2-count1, 1);
+}
+@Test
+public void testNewServicecontracts(){
+	
+	ServicecontractsResource servicecontractsResource = new ServicecontractsResource();
+	Servicecontract servicecontract;
+	List<Servicecontract> servicecontracts =new ArrayList<Servicecontract>();
+	for (int i = 0; i < 10; i++) {
+		servicecontract = new Servicecontract();
+		servicecontract.setCompany(company);
+		servicecontract.setSwauser(swauser);
+	//	ServicecontractDao.getInstance().saveServicecontract(servicecontract);
+		servicecontracts.add(servicecontract);
+	}
+	int count1 = servicecontractsResource.getAllServicecontract().size();
+	
+	//servicecontractsResource.newCompanies(servicecontracts);
+	
+	int count2=servicecontractsResource.getAllServicecontract().size();
+	assertEquals(count2 - servicecontracts.size(),count1);		
+}
+
+
+
+/*public void testGetServicecontract() {
+	ServicecontractDao s = ServicecontractDao.getInstance();
+	Servicecontract servicecontract = new Servicecontract();
+   
+	servicecontract.setCompany(company);
+	servicecontract.setSwauser(swauser);
+	s.saveServicecontract(servicecontract);
+	Servicecontract servicecontracts = s.getServicecontract(servicecontract.getIdservicecontract());
+	assertNotNull(servicecontracts);
+	
+}*/
+
 }
